6e4ae426664d9c4e182908152c6e46c3b3b639a2
Pablo Gerardo <pablo.gerardo@icloud.com>
2022-10-07 12:40:49 -0500

    Splash/Launch Screen

---
+++ app/src/main/java/com/example/samplestickerapp/LaunchScreen.java
@@ -0,0 +1,43 @@
+package com.example.samplestickerapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class LaunchScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_launch_screen);
+
+        //AGREGAR ANIMACIONES
+        Animation animacion1 = AnimationUtils.loadAnimation(this,R.anim.desplazamiento_arriba);
+        Animation animacion2 = AnimationUtils.loadAnimation(this,R.anim.desplazamiento_arriba);
+
+        TextView fromTextView = findViewById(R.id.fromtextView);
+        ImageView AzTechLogo = findViewById(R.id.AzTechLogo);
+        ImageView logoImageView = findViewById(R.id.imageView);
+
+        fromTextView.setAnimation(animacion2);
+        AzTechLogo.setAnimation(animacion2);
+        logoImageView.setAnimation(animacion1);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(LaunchScreen.this,EntryActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 4000);
+    }
+}
