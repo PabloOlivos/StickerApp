193a9b144db04a6253edf320c2c5d0040e014232
Pablo Gerardo <pablo.gerardo@icloud.com>
2022-10-07 14:08:23 -0500

    Login screen

--- app/src/main/java/com/example/samplestickerapp/LaunchScreen.java
+++ app/src/main/java/com/example/samplestickerapp/LaunchScreen.java
@@ -2,9 +2,13 @@ package com.example.samplestickerapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ActivityOptions;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Pair;
+import android.view.View;
 import android.view.WindowManager;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
@@ -16,7 +20,7 @@ public class LaunchScreen extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        //getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_launch_screen);
 
         //AGREGAR ANIMACIONES
@@ -34,9 +38,20 @@ public class LaunchScreen extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent = new Intent(LaunchScreen.this,EntryActivity.class);
-                startActivity(intent);
-                finish();
+                Intent intent = new Intent(LaunchScreen.this,LoginActivity.class);
+
+                Pair[] pairs = new Pair[2];
+                pairs[0] = new Pair<View, String>(logoImageView, "logoImageTrans");
+                pairs[1] = new Pair<View, String>(AzTechLogo, "textTrans");
+
+                if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP){
+                    ActivityOptions options = ActivityOptions.makeSceneTransitionAnimation(LaunchScreen.this, pairs);
+                    startActivity(intent, options.toBundle());
+                }else{
+                    startActivity(intent);
+                    finish();
+                }
+
             }
         }, 4000);
     }
