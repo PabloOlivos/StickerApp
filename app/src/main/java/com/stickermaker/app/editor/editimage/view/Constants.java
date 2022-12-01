5863b732ff94238dda62e1e5f2b078d311d20b1b
Pablo Gerardo <pablo.gerardo@icloud.com>
2022-12-01 13:09:42 -0600

    01/12/2022

--- app/src/main/java/com/stickermaker/app/editor/editimage/view/Constants.java
+++
@@ -1,7 +0,0 @@
-package com.stickermaker.app.editor.editimage.view;
-
-
-
-public class Constants {
-    public static final int STICKER_BTN_HALF_SIZE = 30;
-}
--- app/src/main/java/com/stickermaker/app/services/CallBackBilling.java
+++
@@ -1,7 +0,0 @@
-package com.stickermaker.app.services;
-
-public interface CallBackBilling {
-    void onPurchase();
-    void onNotPurchase();
-    void onNotLogin();
-}
